61fdf67e7fdaabf99ffd750c0e28865a1b0a0503
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2023-08-14 11:29:48 +0200

    WIP | Add function for resize <p> text with limit at 75 caracts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '@/styles/Home.module.scss'
 import Link from "next/link";
 
 const AboutComponent = () => {
+
+  function splitText(text, maxLength) {
+    const result = [];
+    let start = 0;
+
+    while (start < text.length) {
+        let end = start + maxLength;
+        if (end < text.length && text[end] !== ' ') {
+            while (end > start && text[end] !== ' ') {
+                end--;
+            }
+        }
+        result.push(text.substring(start, end));
+        start = end + 1; // +1 pour sauter l'espace
+    }
+
+    return result;
+  }
+
+
+  const originalText = "My experience in front-end development has allowed me to refine my attention to detail and create innovative user interfaces that meet user needs and are a pleasure to use.";
+  const maxLength = 75;
+  const lines = splitText(originalText, maxLength);
+  console.log(lines, 'lines');
+
+
   return (
     <div className={styles.about}>
       <h1 className={styles.about__title}>Hey, I'm REMI JACQUEMIN</h1>
-      <p className={styles.about__desc}>My experience in front-end development has allowed me to refine my attention to detail and create innovative user interfaces that meet user needs and are a pleasure to use.</p>
+      {lines.map((line, index) => (
+        <p key={index} className={styles.about__desc}>{line}</p>
+      ))}
 
       <div className={styles.about__project}>
         <button>
