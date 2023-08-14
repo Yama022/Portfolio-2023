8b51f414cd8b65db0a72593e7f66641338288d73
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2023-08-14 15:29:48 +0200

    WIP | Add experiences on component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,12 +4,49 @@ import styles from "@/styles/Home.module.scss";
 import Header from "./header";
 import Footer from "./footer";
 
-
 const ContactComponent = () => {
   return (
-    <div>
+    <div className={styles.contact}>
       <Header />
-      <h1>Contact</h1>
+
+      <div className={styles.contactHeader}>
+        <h1>Contact</h1>
+        <p>Vous avez des questions ou souhaitez discuter d'un projet ? N'hésitez pas à me contacter !</p>
+      </div>
+
+      <div className={styles.contactForm}>
+        <form>
+          <div>
+            <label htmlFor="name">Nom</label>
+            <input type="text" id="name" placeholder="Votre nom" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Votre email" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" rows="5" placeholder="Votre message"></textarea>
+          </div>
+          <button type="submit">Envoyer</button>
+        </form>
+      </div>
+
+      <div className={styles.contactDetails}>
+        <h2>Mes coordonnées</h2>
+        <p>Email : exemple@email.com</p>
+        <p>Téléphone : +33 1 23 45 67 89</p>
+      </div>
+
+      <div className={styles.socialLinks}>
+        <a href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
+      </div>
+
+      <div className={styles.location}>
+        {/* Si vous avez une carte intégrée, vous pouvez la placer ici */}
+      </div>
 
       <Footer />
     </div>
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import Link from 'next/link'
 import styles from "@/styles/Home.module.scss";
 import Header from "./header";
 import Footer from "./footer";
 
 const ExpComponent = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.experience}>
       <Header />
-      <div className={styles.experience}>
-        <h1 className={styles.experience.title}>Experience</h1>
 
-        <div className={styles.experience.list}>
-            <h2 className={styles.experience.list__title}>Frontend Developer</h2>
-            <h3 className={styles.experience.list__company}>Company Name</h3>
-            <p className={styles.experience.list__date}>2019 - 2020</p>
-            <p className={styles.experience.list__description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam. Sed vitae nisi eget nunc ultricies aliquam.</p>
+      <h1 className={styles.experience__title}>Works Experiences</h1>
 
-        </div>
-
+      {/* Expérience : L'Oréal Projet : Lot n°2 */}
+      <div className={styles.experience__item}>
+        <h2 className={styles.experience__item__role}>Frontend React / Angular Developer</h2>
+        <h3 className={styles.experience__item__company}>L'Oréal - Project : Lot n°2</h3>
+        <p className={styles.experience__item__date}>08/2023 - Present</p>
+        <p className={styles.experience__item__tech}>Technology : Front end: React / Angular, Back end: NodeJS / PHP</p>
+        <p className={styles.experience__item__desc}>Fullstack JS developer on the Digit Expert project at L'Oréal, managing incident tickets and bug fixes before going live.</p>
       </div>
+
+      {/* Expérience : CAPGEMINI - PROJET IDEE */}
+      <div className={styles.experience__item}>
+        <h2 className={styles.experience__item__role}>Frontend React Developer</h2>
+        <h3 className={styles.experience__item__company}>CAPGEMINI - PROJET IDEE</h3>
+        <p className={styles.experience__item__date}>05/2023 - 08/2023</p>
+        <p className={styles.experience__item__tech}>Technology : HTML, SCSS, Javascript, React</p>
+        <p className={styles.experience__item__desc}>Front end React developer for R&I CapGemini on the IDEE - miniCV project. Component development in a team of 2 frontend developers and 1 backend developer.</p>
+      </div>
+
+      {/* Expérience : CAPGEMINI - ALSTOM */}
+      <div className={styles.experience__item}>
+        <h2 className={styles.experience__item__role}>Full Stack Developer</h2>
+        <h3 className={styles.experience__item__company}>CAPGEMINI - ALSTOM</h3>
+        <p className={styles.experience__item__date}>11/2022 - 05/2023</p>
+        <p className={styles.experience__item__tech}>Technology : HTML, SCSS, Javascript, Angular, NodeJS, SQL</p>
+        <p className={styles.experience__item__desc}>Creation of a web platform to connect 3D printers.</p>
+      </div>
+
+      {/* Expérience : ExplorAstro */}
+      <div className={styles.experience__item}>
+        <h2 className={styles.experience__item__role}>Frontend Developer</h2>
+        <h3 className={styles.experience__item__company}>ExplorAstro</h3>
+        <p className={styles.experience__item__date}>2021</p>
+        <p className={styles.experience__item__tech}>Technology : HTML, SCSS, Javascript, React (avec Redux), Bulma</p>
+        <p className={styles.experience__item__desc}>Development of the first social network for astronomy enthusiasts.</p>
+      </div>
+
+
+
+
+
+
 
       <Footer />
     </div>
