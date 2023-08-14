6bc46bf67966c9b6c72199e4bd710a7a9f858c35
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2023-08-14 10:54:21 +0200

    FIX | Problem version Next.js + dependancies

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -8,8 +8,16 @@ const ExpComponent = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <div className="styles__experience">
-        <h1>Experience</h1>
+      <div className={styles.experience}>
+        <h1 className={styles.experience.title}>Experience</h1>
+
+        <div className={styles.experience.list}>
+            <h2 className={styles.experience.list__title}>Frontend Developer</h2>
+            <h3 className={styles.experience.list__company}>Company Name</h3>
+            <p className={styles.experience.list__date}>2019 - 2020</p>
+            <p className={styles.experience.list__description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam. Sed vitae nisi eget nunc ultricies aliquam.</p>
+
+        </div>
 
       </div>
 
